8b0051f753541aaf75777e39f3faf6e135e7e2e4
Maciej Caderek <maciej.caderek@gmail.com>
2023-12-04 02:48:57 +0100

    Added template

---
+++ day-template/main.lua
@@ -0,0 +1,29 @@
+io.input("./day00/input.txt")
+
+local lines = {}
+
+for line in io.lines() do
+  table.insert(lines, line)
+end
+
+-- PARSING
+
+local something = {}
+
+for _, line in ipairs(lines) do
+  table.insert(something, nil)
+end
+
+-- HELPERS
+
+-- PART 1
+
+local solution1 = 0
+
+print("Part 1:", solution1)
+
+-- PART 2
+
+local solution2 = 0
+
+print("Part 2:", solution2)
