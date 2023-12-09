aeed3c0c2caaa55d5019502aad2d542d0dde0fb9
Maciej Caderek <maciej.caderek@gmail.com>
2023-12-09 06:40:05 +0100

    Solutions day 9

--- day09/main.lua
+++ day09/main.lua
@@ -1,8 +1,7 @@
 require("utils.pretty-print")
 local util = require("utils.util")
 
--- local raw = util.readInput()
-local raw = util.readInput("example.txt")
+local raw = util.readInput()
 
 -- PARSING
 
@@ -10,16 +9,50 @@ local input = {}
 
 do
   for _, line in ipairs(util.split(raw, "\n")) do
-    table.insert(input, line)
+    table.insert(input, util.map(util.toList(line:gmatch("[-]*%d+")), util.toDecimal))
   end
 end
 
-print(input)
-
 -- PART 1
+
+local function isZero(x)
+  return x == 0
+end
+
+local function extrapolate(nums)
+  if util.every(nums, isZero) then
+    return nums[#nums]
+  end
+
+  local next = {}
+
+  for i = 1, #nums - 1 do
+    next[i] = nums[i + 1] - nums[i]
+  end
+
+  return nums[#nums] + extrapolate(next)
+end
+
+local function extrapolateBackwards(nums)
+  if util.every(nums, isZero) then
+    return nums[1]
+  end
+
+  local next = {}
+
+  for i = 1, #nums - 1 do
+    next[i] = nums[i + 1] - nums[i]
+  end
+
+  return nums[1] - extrapolateBackwards(next)
+end
 
 do
   local solution1 = 0
+
+  for _, nums in ipairs(input) do
+    solution1 = solution1 + extrapolate(nums)
+  end
 
   print("Part 1:", solution1)
 end
@@ -28,6 +61,10 @@ end
 
 do
   local solution2 = 0
+
+  for _, nums in ipairs(input) do
+    solution2 = solution2 + extrapolateBackwards(nums)
+  end
 
   print("Part 2:", solution2)
 end
