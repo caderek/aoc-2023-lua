227d5e1aceeea7105f1a9af0b97afc272d10a684
Maciej Caderek <maciej.caderek@gmail.com>
2023-12-09 07:13:54 +0100

    Refactored day 9 extrapolation to single function

--- day09/main.lua
+++ day09/main.lua
@@ -13,17 +13,18 @@ do
   end
 end
 
--- HELPERS
+-- COMMON
 
 local function isZero(x)
   return x == 0
 end
 
--- PART 1
+local function extrapolate(nums, backwards)
+  local sign = backwards and -1 or 1
+  local index = backwards and 1 or #nums
 
-local function extrapolate(nums)
   if util.every(nums, isZero) then
-    return nums[#nums]
+    return 0
   end
 
   local next = {}
@@ -32,8 +33,10 @@ local function extrapolate(nums)
     next[i] = nums[i + 1] - nums[i]
   end
 
-  return nums[#nums] + extrapolate(next)
+  return nums[index] + extrapolate(next, backwards) * sign
 end
+
+-- PART 1
 
 do
   local solution1 = 0
@@ -47,25 +50,11 @@ end
 
 -- PART 2
 
-local function extrapolateBackwards(nums)
-  if util.every(nums, isZero) then
-    return nums[1]
-  end
-
-  local next = {}
-
-  for i = 1, #nums - 1 do
-    next[i] = nums[i + 1] - nums[i]
-  end
-
-  return nums[1] - extrapolateBackwards(next)
-end
-
 do
   local solution2 = 0
 
   for _, nums in ipairs(input) do
-    solution2 = solution2 + extrapolateBackwards(nums)
+    solution2 = solution2 + extrapolate(nums, true)
   end
 
   print("Part 2:", solution2)
